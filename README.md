b3cef8ef5d98a9d6b2c529c09eaef510a3c73553
Aurimas Niekis <aurimas@niekis.lt>
2019-12-21 06:21:20 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# RSPeer Client
+
+RSPeer Client Library Mirror Maven Dependency
+
+# Usage
+
+Setup Github Packages settings on your `.m2/settings.xml` see [here](https://help.github.com/en/github/managing-packages-with-github-packages/configuring-apache-maven-for-use-with-github-packages#authenticating-to-github-packages)
+
+```xml
+<dependency>
+  <groupId>org</groupId>
+  <artifactId>rspeer</artifactId>
+  <version>2.12</version>
+</dependency>
+```
---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+CURRENT_VERSION_FILE=".currentVersion"
+
+LATEST_VERSION=$(curl "http://services.rspeer.org/api/bot/currentVersion?game=0" | jq ".version")
+CURRENT_VERSION=""
+
+if [ -r $CURRENT_VERSION_FILE ]; then
+  printf "Reading current version file\n"
+  CURRENT_VERSION=$(cat $CURRENT_VERSION_FILE)
+fi
+
+printf "Current Version: \"%s\"\n" "$CURRENT_VERSION"
+printf "Latest Version: \"%s\"\n" "$LATEST_VERSION"
+if [ "$LATEST_VERSION" != "$CURRENT_VERSION" ]; then
+  printf "Downloading latest version\n"
+  curl "https://services.rspeer.org/v2/bot/currentJar?game=186" --output rspeer.jar
+  echo "$LATEST_VERSION" >$CURRENT_VERSION_FILE
+
+  curl -X PUT \
+    "https://maven.pkg.github.com/Arbu7as-OSRS/rspeer-client/org/rspeer/$LATEST_VERSION/rspeer-$LATEST_VERSION.jar" \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    --upload-file "./rspeer.jar" -vvv
+
+    REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+    git config --local user.email "action@github.com"
+    git config --local user.name "GitHub Action"
+    git commit -m "Updated Current Version file" -a
+    git push "${REMOTE_REPO}" HEAD:master --follow-tags
+    return 0
+fi
+
+return 0
